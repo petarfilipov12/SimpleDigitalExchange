000d77ccc96b4c1ba32d4ce55776c4d5ab930f92
petarfilipov12 <petar.filipov12@gmail.com>
2025-04-15 17:40:24 +0300

    Split code to .h and .cpp files

--- matching_engine/code/engine/engine.h
+++ matching_engine/code/engine/engine.h
@@ -1,17 +1,12 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
-#include <iostream>
-#include <string>
-
 #include "types.h"
 #include "order_book.h"
 #include "market_order_book.h"
 #include "order.h"
 #include "event.h"
 #include "event_bus.h"
-
-using namespace std;
 
 class Engine{
     private:
@@ -20,205 +15,29 @@ class Engine{
         EventBus *event_bus;
     
     public:
-        Engine(EventBus *event_bus)
-        {
-            this->event_bus = event_bus;
-        }
+        Engine(EventBus *event_bus);
         
-        ~Engine()
-        {
-            // this->market_orders_queue.clear();
-            // this->market_orders.clear();
+        ~Engine();
         
-            // this->order_book.~OrderBook();
-        }
+        bool ExistsOrder(Order order) const;
         
-        bool ExistsOrder(Order order) const
-        {
-            bool ret = false;
+        bool ExistsOrderId(int id) const;
         
-            if (order.order_type == ORDER_TYPE_MARKET)
-            {
-                ret = this->market_book.ExistsMarketOrder(order);
-            }
-            else if (order.order_type == ORDER_TYPE_LIMIT)
-            {
-                ret = this->order_book.ExistsOrder(order);
-            }
-            else
-            {
-                // Nothing to do
-            }
+        Return_Type AddOrder(Order order);
         
-            return ret;
-        }
+        Return_Type CancelOrder(Order order);
         
-        bool ExistsOrderId(int id) const
-        {
-            bool ret = this->market_book.ExistsMarketOrderById(id);
+        Return_Type CancelOrderById(int id);
         
-            if (!ret)
-            {
-                ret = this->order_book.ExistsOrderId(id);
-            }
+        void PrintOrderBook();
         
-            return ret;
-        }
+        void MatchOrderBook();
         
-        Return_Type AddOrder(Order order)
-        {
-            Return_Type ret = RET_NOT_OK;
-
-            if (order.order_type == ORDER_TYPE_MARKET)
-            {
-                ret = this->market_book.AddMarketOrder(order);
-            }
-            else if (order.order_type == ORDER_TYPE_LIMIT)
-            {
-                ret = this->order_book.AddOrder(order);
-            }
-            else
-            {
-                // Error
-            }
-
-            return ret;
-        }
+        void MatchMarketOrder();
         
-        Return_Type CancelOrder(Order order)
-        {
-            Return_Type ret = RET_NOT_OK;
-
-            if (order.order_type == ORDER_TYPE_MARKET)
-            {
-                ret = this->market_book.CancelMarketOrder(order);
-            }
-            else if (order.order_type == ORDER_TYPE_LIMIT)
-            {
-                ret = this->order_book.CancelOrder(order);
-            }
-            else
-            {
-                // Error
-            }
-
-            return ret;
-        }
+        void Cyclic();
         
-        Return_Type CancelOrderById(int id)
-        {
-            Return_Type ret = RET_NOT_OK;
-
-            if (this->market_book.ExistsMarketOrderById(id))
-            {
-                ret = this->market_book.CancelMarketOrderById(id);
-            }
-            else
-            {
-                ret = this->order_book.CancelOrderById(id);
-            }
-
-            return ret;
-        }
-        
-        void PrintOrderBook()
-        {
-            this->order_book.PrintOrderBook();
-        }
-        
-        void MatchOrderBook()
-        {
-            Order bid_order;
-            Order ask_order;
-
-            if(
-                (RET_OK == this->order_book.GetBidFirst(&bid_order)) &&
-                (RET_OK == this->order_book.GetAskFirst(&ask_order))
-            )
-            {
-                if (stof(bid_order.price) >= stof(ask_order.price))
-                {
-                    cout << "BOOK Order FILL: " << endl;
-                    bid_order.PrintOrder();
-                    ask_order.PrintOrder();
-
-                    json j_data;
-                    j_data["price"] = ask_order.price;
-                    j_data["bid_order_id"] = bid_order.id;
-                    j_data["ask_order_id"] = ask_order.id;
-
-                    Event event(EVENT_ID_ORDER_FILL, j_data);
-                    this->event_bus->Send(event);
-
-                    this->order_book.CancelOrderById(bid_order.id);
-                    this->order_book.CancelOrderById(ask_order.id);
-                }
-            }
-        }
-        
-        void MatchMarketOrder()
-        {
-            Order market_order;
-            Order book_order;
-            json j_data;
-        
-            if(RET_OK != this->market_book.GetFirst(&market_order))
-            {
-                return;
-            }
-
-            if (market_order.order_side == ORDER_SIDE_BUY)
-            {
-                if(RET_OK != this->order_book.GetAskFirst(&book_order))
-                {
-                    return;
-                }
-
-                j_data["ask_order_id"] = book_order.id;
-                j_data["bid_order_id"] = market_order.id;
-            }
-            else if (market_order.order_side == ORDER_SIDE_SELL)
-            {
-                if(RET_OK != this->order_book.GetBidFirst(&book_order))
-                {
-                    return;
-                }
-
-                j_data["ask_order_id"] = market_order.id;
-                j_data["bid_order_id"] = book_order.id;
-            }
-            else
-            {
-                this->market_book.PopFirst();
-        
-                return;
-            }
-            j_data["price"] = book_order.price;
-        
-            cout << "Market Order FILL: " << endl;
-            market_order.PrintOrder();
-            book_order.PrintOrder();
-
-            Event event(EVENT_ID_ORDER_FILL, j_data);
-            this->event_bus->Send(event);
-        
-            this->order_book.CancelOrderById(book_order.id);
-            this->market_book.PopFirst();
-        }
-        
-        void Cyclic()
-        {
-            this->MatchOrderBook();
-            this->MatchMarketOrder();
-        }
-        
-        void run()
-        {
-            while (true)
-            {
-                this->Cyclic();
-            }
-        }
+        void run();
 };
 
 #endif
--- matching_engine/code/order/order.h
+++ matching_engine/code/order/order.h
@@ -1,12 +1,21 @@
 #ifndef ORDER_H
 #define ORDER_H
 
-#include <iostream>
-#include <functional>
-
-#include "order_enums.h"
+#include <string>
 
 using namespace std;
+
+enum OrderSide{
+    ORDER_SIDE_BUY,
+    ORDER_SIDE_SELL,
+    ORDER_SIDE_INVALID
+};
+
+enum OrderType{
+    ORDER_TYPE_MARKET,
+    ORDER_TYPE_LIMIT,
+    ORDER_TYPE_INVALID
+};
 
 class Order{
     public:
@@ -15,82 +24,29 @@ class Order{
         enum OrderSide order_side;
         enum OrderType order_type;
     
-        Order(){
-            this->price = "0.0";
-            this->id = 0;
-            this->order_side = ORDER_SIDE_INVALID;
-            this->order_type = ORDER_TYPE_INVALID;
-        }
+        Order();
 
-        Order(int id){
-            this->price = "0.0";
-            this->id = id;
-            this->order_side = ORDER_SIDE_INVALID;
-            this->order_type = ORDER_TYPE_INVALID;
-        }
+        Order(int id);
 
-        Order(string price, int id, enum OrderSide order_side, enum OrderType order_type){
-            this->price = price;
-            this->id = id;
-            this->order_side = order_side;
-            this->order_type = order_type;
-        }
+        Order(string price, int id, enum OrderSide order_side, enum OrderType order_type);
 
         // bool operator<(Order order2) const{
         //     return this->id < order2.id;
         // }
 
-        Order& operator=(const Order& order2){
-            this->price = order2.price;
-            this->id = order2.id;
-            this->order_side = order2.order_side;
-            this->order_type = order2.order_type;
+        Order& operator=(const Order& order2);
 
-            return *this;
-        }
-
-        bool operator==(const Order& order2) const{
-            return this->id == order2.id;
-        }
+        bool operator==(const Order& order2) const;
 
         class HashFunc{
             public:
-                size_t operator()(const Order& order) const{
+                size_t operator()(const Order& order) const
+                {
                     return hash<int>{}(order.id);
                 }
         };
 
-        void PrintOrder(){
-            string s_order_side, s_order_type;
-
-            if(this->order_side == ORDER_SIDE_BUY){
-                s_order_side = "ORDER_SIDE_BUY";
-            }
-            else if(this->order_side == ORDER_SIDE_SELL){
-                s_order_side = "ORDER_SIDE_SELL";
-            }
-            else if(this->order_side == ORDER_SIDE_INVALID){
-                s_order_side = "ORDER_SIDE_INVALID";
-            }
-            else{
-                s_order_side = "NA";
-            }
-
-            if(this->order_type == ORDER_TYPE_MARKET){
-                s_order_type = "ORDER_TYPE_MARKET";
-            }
-            else if(this->order_type == ORDER_TYPE_LIMIT){
-                s_order_type = "ORDER_TYPE_LIMIT";
-            }
-            else if(this->order_type == ORDER_TYPE_INVALID){
-                s_order_type = "ORDER_TYPE_INVALID";
-            }
-            else{
-                s_order_type = "NA";
-            }
-
-            cout << "id=" << this->id << ", price=" << this->price << ", side=" << s_order_side << ", type=" << s_order_type << endl;
-        }
+        void PrintOrder();
 };
 
 #endif
